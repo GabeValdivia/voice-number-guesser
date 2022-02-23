a3bad91ec28f23e5816c6fa82591e782e6fa48d5
GabeValdivia <gabe@gabeswebcoding.com>
2022-02-22 20:13:26 -0600

    Start recognition and console log object

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ const msgEl = document.getElementById('msg');
 
 const randomNum = getRandomNumber();
 
-
-
 console.log(`Number: ${randomNum}`);
-
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 
-let recongnition = new window.SpeechRecognition();
+let recognition = new window.SpeechRecognition();
 
 // Start Recognition and game
-recongnition.start();
+recognition.start();
 
+// Capture user speak
+function onSpeak(e) {
+	console.log(e);
+}
+
+
+// Generate random number
 function getRandomNumber() {
 	return Math.floor(Math.random() * 100) + 1;
 }
+
+// Speak result
+recognition.addEventListener('result', onSpeak);
