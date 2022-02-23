12b05afd532d35617bb26524c1705e40f6bc8334
GabeValdivia <gabe@gabeswebcoding.com>
2022-02-22 20:09:04 -0600

    Initiate speechRecongnition

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const msgEl = document.getElementById('msg');
 
 const randomNum = getRandomNumber();
 
-console.log(randomNum);
+
+
+console.log(`Number: ${randomNum}`);
+
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+let recongnition = new window.SpeechRecognition();
+
+// Start Recognition and game
+recongnition.start();
 
 function getRandomNumber() {
 	return Math.floor(Math.random() * 100) + 1;
